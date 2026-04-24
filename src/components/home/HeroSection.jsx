226f4a6c589592bbef3dd37a88484705e8e64135
juanchoalcoba <juanchoalcoba@gmail.com>
2026-04-24 15:18:10 -0300

    update

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -84,7 +84,7 @@ export default function HeroSection() {
           </div>
 
           {/* Heading */}
-          <h1 className="text-5xl sm:text-6xl lg:text-7xl xl:text-[5.5rem] font-black leading-[1.1] lg:leading-[0.95] tracking-tight text-white mb-6 drop-shadow-md flex flex-col items-center lg:items-start">
+          <h1 className="text-5xl sm:text-6xl lg:text-7xl xl:text-[5.5rem] font-display font-black leading-[1.1] lg:leading-[0.95] tracking-tight text-white mb-6 drop-shadow-md flex flex-col items-center lg:items-start">
             <span className="mb-1 lg:mb-0">TELEREMATE </span>
 
             <span
